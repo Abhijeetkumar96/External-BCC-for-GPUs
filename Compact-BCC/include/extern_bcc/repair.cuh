2d4c80ad2b9d9bb81f7e0f170605c49b2812dc64
Abhijeet <43548725+Abhijeetkumar96@users.noreply.github.com>
2025-05-11 17:06:59 +0530

    Cleaned main branch: keep only Fast-BCC code

--- Compact-BCC/include/extern_bcc/repair.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef REPAIR_H
-#define REPAIR_H
-
-#include "extern_bcc/bcg_memory_utils.cuh"
-
-void repair(GPU_BCG& g_bcg_ds, const int& root, const int child_of_root, const int totalRootChild, cudaStream_t myStream);
-
-#endif // REPAIR_H
